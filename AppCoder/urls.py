7220559c8bf0915b58c808039d0e141310b87537
Tiago <disalviotiago@gmail.com>
2022-06-18 22:54:17 -0300

    update

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -4,10 +4,10 @@ from .views import buscar, busquedaUsuario, inicio, pedido_formulario, producto_
 
 
 urlpatterns = [
-    path("", inicio, name='inicio'),
-    path("userFormulario/", user_formulario, name='user_formulario'),
-    path("productoFormulario/", producto_formulario, name='producto_formulario'),
-    path("pedidoFormulario/", pedido_formulario, name='pedido_formulario'),
-    path("busquedaUsuario/", busquedaUsuario, name='busqueda_usuario'),
-    path("buscar/", buscar),
+    path('', inicio, name='inicio'),
+    path('userFormulario/', user_formulario, name='user_formulario'),
+    path('productoFormulario/', producto_formulario, name='producto_formulario'),
+    path('pedidoFormulario/', pedido_formulario, name='pedido_formulario'),
+    path('busquedaUsuario/', busquedaUsuario, name='busqueda_usuario'),
+    path('buscar/', buscar),
 ]
