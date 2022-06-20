44c54191f8a362d6c963296fc6ce17f81d877cb8
Tiago <disalviotiago@gmail.com>
2022-06-20 20:59:51 -0300

    update

--- ProyectoCoder/urls.py
+++ ProyectoCoder/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin", admin.site.urls),
     path('', include('AppCoder.urls')),
-    
+    path('AppCoder/', include('AppCoder.urls')),
     
 ]
